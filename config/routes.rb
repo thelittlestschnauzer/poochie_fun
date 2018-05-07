4242399ce699d0ed80e8a5235ffe63cec1c794a7
github username <github email address>
2018-05-07 19:23:39 -0400

    playing with routes/paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'poochies/home'
-  root 'application#hello'
+  root 'poochies#home'
+  get '/about', to: 'poochies#about'
+  get '/contact', to: 'poochies#contact'
+  get '/login', to: 'poochies#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
