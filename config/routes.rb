ad6bea2b42eb2a1586be0d04aa57f3b2141324f4
github username <github email address>
2018-05-08 11:04:31 -0400

    Added a sign up page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/about', to: 'poochies#about'
   get '/contact', to: 'poochies#contact'
   get '/signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   get '/login', to: 'poochies#login'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
