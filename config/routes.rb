1ba3b30b82433705e87286d314cf86e7908851b8
github username <github email address>
2018-05-08 10:03:51 -0400

    Generated the Users Controller and route for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'poochies#home'
   get '/about', to: 'poochies#about'
   get '/contact', to: 'poochies#contact'
+  get '/signup', to: 'users#new'
   get '/login', to: 'poochies#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
